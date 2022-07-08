b5d1654a726ee7059932df732b0905a771f7fc42
Francisco Pugh <franciscopugh01@gmail.com>
2022-07-07 23:50:49 -0300

    Clase 1

---
+++ clase-1/script.js
@@ -0,0 +1,31 @@
+/*var nombre = 'Francisco'; //String 
+var apellido = "Pugh"; 
+var edad = 38; //Number
+var es_profesor_de_coder = true; //Boolean o Logico
+var esPadre = undefined; //Indefinado
+let sueldo = null; //Vacio
+const IVA = 1.21 
+
+IVA = 1.30
+console.log(nombre)
+console.log(sueldo)
+
+sueldo = 10
+var nombre = 5
+
+console.log(sueldo)
+console.log(nombre)
+
+let nombre = prompt("Ingrese su nombre")
+alert(nombre)
+console.log(nombre)*/
+
+let numero1 = parseInt(prompt("Ingrese un numero"));
+let numero2 = parseFloat(prompt("Ingrese un numero"));
+
+/*
+console.log(5 + 5) //10, entonces los sumo
+console.log("5" + "5") //"55", entonces los concateno
+console.log("Francisco" + " Pugh") //"Francisco Pugh"
+console.log("5" + 5) //10 o "55"*/
+console.log(numero1 + numero2);
